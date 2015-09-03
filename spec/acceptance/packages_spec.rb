c9ae38e3061a8de809f9a00a4427d99644aab619
Glauco Oliveira <gvinici@thoughtworks.com>
2015-09-03 15:04:19 -0300

    Add acceptance tests for puppet-diskimage_builder.
    Add acceptance tests for puppet-diskimage_builder module so that once
    the module is applied we check if packages were installed.
    
    During the testing, we noticed that apt::ppa was failing for two
    reasons, it was being used without including the module and it was not
    updating the package list after including the ppa.
    
    We then included the apt module in the beginning and required an apt
    update before installing the packages.
    
    Change-Id: I37a7e4ae9f1a380568c0aa4f09cef1dfb7c4aaa9
    Co-Authored-By: Bruno Tavares <btavare@thoughtworks.com>

---
+++ spec/acceptance/packages_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper_acceptance'
+
+describe 'packages', :if => ['debian', 'ubuntu'].include?(os[:family]) do
+  describe 'required OS packages' do
+    required_packages = [
+      package('debian-keyring'),
+      package('debootstrap'),
+      package('kpartx'),
+      package('python-lzma'),
+      package('python-yaml'),
+      package('qemu-utils'),
+      package('ubuntu-keyring'),
+      package('vhd-util'),
+      package('yum'),
+      package('yum-utils'),
+    ]
+
+    required_packages.each do |package|
+      describe package do
+        it { should be_installed }
+      end
+    end
+  end
+
+  describe 'required Python packages' do
+    describe package('diskimage-builder') do
+      it { should be_installed.by('pip') }
+    end
+  end
+
+  describe ppa('openstack-ci-core/vhd-util') do
+    it { should exist }
+    it { should be_enabled }
+  end
+end
