2a33304ee5548b3ba836c204ee2b52c1f3058511
Glauco Oliveira <gvinici@thoughtworks.com>
2015-09-03 15:04:19 -0300

    Add acceptance tests for puppet-diskimage_builder.
    Add acceptance tests for puppet-diskimage_builder module so that once
    the module is applied we check if packages were installed.
    
    Change-Id: I37a7e4ae9f1a380568c0aa4f09cef1dfb7c4aaa9
    Co-Authored-By: Bruno Tavares <btavare@thoughtworks.com>
    Co-Authored-By: Danilo Ramalho <dramalho@thoughtworks.com>

---
+++ spec/acceptance/fixtures/preconditions.pp
@@ -0,0 +1,20 @@
+package { 'ssl-cert':
+  ensure => present,
+}
+
+exec { 'download get-pip.py':
+  command => 'wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py',
+  path    => '/bin:/usr/bin:/usr/local/bin',
+  creates => '/tmp/get-pip.py',
+}
+
+exec { 'install pip using get-pip':
+  command     => 'python /tmp/get-pip.py',
+  path        => '/bin:/usr/bin:/usr/local/bin',
+  refreshonly => true,
+  subscribe   => Exec['download get-pip.py'],
+}
+
+package { 'software-properties-common':
+  ensure => present,
+}
